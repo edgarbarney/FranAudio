ad3e7bede4d92b715616511e61889467c33bde31
FranticDreamer <edwardgmgmd@gmail.com>
2025-08-25 18:42:07 +0300

    Add Path Prefix Removal to File Copy Macro

--- CMakeMacros.cmake
+++ CMakeMacros.cmake
@@ -12,16 +12,23 @@ endmacro()
 
 
 # Export files
-macro (COPY_FILES OUTPUT_TARGET COPY_FILE_LIST_VAR OUTPUT_DIR)
+macro (COPY_FILES OUTPUT_TARGET COPY_FILE_LIST_VAR OUTPUT_DIR REMOVE_PATH_PREFIX)
 	message (STATUS "=========================================")
 	message (STATUS "Copying files for ${OUTPUT_TARGET}")
+	
+	# Default value for REMOVE_PATH_PREFIX if not provided
+	if (NOT REMOVE_PATH_PREFIX)
+		set(REMOVE_PATH_PREFIX "")
+	endif()
 
 	foreach (header ${${COPY_FILE_LIST_VAR}})
 		# Compute RELATIVE path
 		file (RELATIVE_PATH relpath
-			${CMAKE_CURRENT_SOURCE_DIR}
+			${CMAKE_CURRENT_SOURCE_DIR}/${REMOVE_PATH_PREFIX}  # Include the prefix to remove
 			${header}  
 		)
+
+		# Ensure the directory structure is created
 		get_filename_component (dir ${header} DIRECTORY)
 		get_filename_component (relpathdir ${relpath} DIRECTORY)
 
@@ -38,3 +45,4 @@ macro (COPY_FILES OUTPUT_TARGET COPY_FILE_LIST_VAR OUTPUT_DIR)
 	message (STATUS "Files copied for ${OUTPUT_TARGET}")
 	message (STATUS "=========================================")
 endmacro()
+
